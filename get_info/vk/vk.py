02ca3faba797d27899c70566a20c380788d9c60a
Artemii <angap4@gmail.com>
2025-03-31 22:37:04 +1000

    class added

--- get_info/vk/vk.py
+++ get_info/vk/vk.py
@@ -1,39 +1,52 @@
 import vk_api
+import time
 from dotenv import dotenv_values
 
 secrets = dotenv_values(".env")
 """Секреты"""
 
-vk = vk_api.VkApi(token=secrets["VK_TOKEN"])
-"""Модуль ВК"""
+# vk = vk_api.VkApi(token=secrets["VK_TOKEN"])
+# """Модуль ВК"""
 
 
-def search_feed(
-    q: str,
-    count: int,
-    list: int = 0,
-    geo: tuple[int, int] | None = None,
-    time: tuple[int | None, int | None] | None = None,
-):
-    params = {"q": q, "count": count, "start_from": list}
-    # ! Времянка
-    if geo:
-        params["geo"] = geo
-    if time:
-        params["time"] = time
-    return vk.method("newsfeed.search", values=params)
+class VK_parser:
+    def __init__(self, vk_token: str | tuple[str, str]):
+        if isinstance(vk_token, str):
+            self.vk = vk_api.VkApi(token=vk_token)
+        elif isinstance(vk_token, tuple[str, str]):
+            self.vk = vk_api.VkApi(*vk_token)
+        else:
+            raise TypeError
 
+    def search_feed(
+        self,
+        q: str,
+        count: int = 200,
+        start_from: int = 0,
+        time_start: int | None = 0,
+        time_end: int = int(time.time()),
+    ):
+        params = {
+            "q": q,
+            "count": count,
+            "start_from": start_from,
+            "time_start": time_start,
+            "time_end": time_end,
+            "extended": True,
+        }
+        return self.vk.method("newsfeed.search", values=params)
 
-# Обратите внимание — даже при использовании параметра offset для получения информации доступны только первые 1000 результатов.
-# start_from - offset по страницам.
-# Поэкспериментировать с offset и start_from, потому как они, похоже, не эквивалентны
-# Или просто реальное количество записей не совпадает с видимым
-# Или ограничение на 1000 записей очень жёсткое
 # print(len(search_feed("кино", 1)['items']))
 # print(search_feed("кино", 1, 0)['items'][0]['id'], search_feed("кино", 1, 1)['items'][0]['id'])
-zp = search_feed("кино", 1, 199)
+
+vk = VK_parser(secrets["VK_TOKEN"])
+zp = vk.search_feed("кино")
 print(len(zp["items"]))
 for i in zp["items"]:
     print(i["id"])
+    
+386598
+62063
 
-# total_count не соответствует настоящему count-у
+4958021
+4958002
