0796c229da992ad9c2cd096d11193b3d238e9fbc
Artemii <angap4@gmail.com>
2025-04-21 18:43:09 +1000

    more params to the method parse

--- get_info/abstract/base.py
+++ get_info/abstract/base.py
@@ -3,17 +3,22 @@ from abc import ABC, abstractmethod
 
 class Parser(ABC):
     @abstractmethod
-    def parse(self, q: str | list[str]) -> dict[str, str | int | float | None]:
+    def parse(
+        self, q: str | list[str], oldest_date: int = 0, count_items: int = -1
+    ) -> list[dict[str, str | int | float | None]]:
         """
         Получение информации с сервиса по запросу.
-        
+
         Args:
             q (str | list[str]): Информация, необходимая для поиска объекта в сервисе.
+            oldest_date (int | None): Время самого позднего сообщения в формате timestamp. Defaults to 0.
+            count_items: Максимальное количество возвращаемых элементов. Для получения всех используется значение -1. Defaults to -1
 
         Returns:
             dict[str,str|int|float|None]: Словарь с данными. Структура:
             ```
             {
+                "service_id" (int): Внутренний индекс сервиса
                 "name" (str): Имя пользователя. Для Telegram и VK хранить id пользователя.
                 "additional_id" (str | None): Дополнительный идентификатор для уточнения сообщения (пример: канал в Telegram).
                 "date" (int): Дата в формате timestamp.
@@ -22,11 +27,12 @@ class Parser(ABC):
                 "answer" (str | None): Ответ на отзыв (если присутствует).
             }
             ```
-        
+
         Examples:
-            **Вывод с карт:**
+            **Вывод с Google карт:**
             ```
             {
+                "service_id": 0
                 "name": "Иван Иванов",
                 "additional_id": None,
                 "date": 1678901234,
@@ -35,10 +41,11 @@ class Parser(ABC):
                 "answer": "Спасибо за отзыв!",
             }
             ```
-            
+
             **Вывод с Telegram:**
             ```
             {
+                "service_id": 4
                 "name": "123",
                 "additional_id": "457",
                 "date": 1678901234,
@@ -47,6 +54,6 @@ class Parser(ABC):
                 "answer": None,
             }
             ```
-        
+
         """
         pass
