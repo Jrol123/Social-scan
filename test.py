8a0e61111fca86105205b32764c039112e695915
Artemii <angap4@gmail.com>
2025-04-21 22:57:52 +1000

    vk parser fully done

--- test.py
+++ test.py
@@ -4,6 +4,6 @@ from get_info.vk import VKParser
 secrets = dotenv_values()
 """Секреты"""
 vk = VKParser(secrets["VK_TOKEN"])
-zp = vk.parse(count_items=20, q='Mriya Resort (Крым | Ялта) -купить', return_count=False)
+zp = vk.parse(count_items=-1, q='Mriya Resort (Крым | Ялта) -купить', return_count=False)
 zp
 print(zp)
