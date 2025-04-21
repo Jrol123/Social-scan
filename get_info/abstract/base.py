786c1f385ed285808d91060752f933817198a157
Artemii <angap4@gmail.com>
2025-04-21 19:29:41 +1000

    Finished polishing abstract class

--- get_info/abstract/base.py
+++ get_info/abstract/base.py
@@ -1,21 +1,42 @@
 from abc import ABC, abstractmethod
+from datetime import datetime
 
 
 class Parser(ABC):
+    def __init__(self, service_id: int):
+        self.service_id = service_id
+
+    def __date_convert(self, date_datetime: datetime) -> int:
+        """
+        Конвертация даты в формат timestamp.
+        
+        Требуется для некоторых парсеров
+
+        Args:
+            date_datetime (datetime): Время в формате datetime.
+
+        Returns:
+            int: Время в формате timestamp.
+        """
+        return int(date_datetime.timestamp())
+
     @abstractmethod
     def parse(
-        self, q: str | list[str], oldest_date: int = 0, count_items: int = -1
+        self,
+        q: str | list[str],
+        oldest_date: datetime = datetime(0, 0, 0),
+        count_items: int = -1,
     ) -> list[dict[str, str | int | float | None]]:
         """
         Получение информации с сервиса по запросу.
 
         Args:
             q (str | list[str]): Информация, необходимая для поиска объекта в сервисе.
-            oldest_date (int | None): Время самого позднего сообщения в формате timestamp. Defaults to 0.
-            count_items: Максимальное количество возвращаемых элементов. Для получения всех используется значение -1. Defaults to -1
+            oldest_date (datetime): Время самого позднего сообщения в формате datetime. Defaults to ```datetime(0, 0, 0)```.
+            count_items (int): Максимальное количество возвращаемых элементов. Для получения всех используется значение -1. Defaults to -1
 
         Returns:
-            dict[str,str|int|float|None]: Словарь с данными. Структура:
+            list[dict[str,str|int|float|None]]: Список сообщений. Каждое сообщение - словарь с данными. Структура сообщения:
             ```
             {
                 "service_id" (int): Внутренний индекс сервиса
@@ -31,28 +52,34 @@ class Parser(ABC):
         Examples:
             **Вывод с Google карт:**
             ```
-            {
-                "service_id": 0
-                "name": "Иван Иванов",
-                "additional_id": None,
-                "date": 1678901234,
-                "rating": 5.0,
-                "text": "Отличный сервис!",
-                "answer": "Спасибо за отзыв!",
-            }
+            [
+                {
+                    "service_id": 0
+                    "name": "Иван Иванов",
+                    "additional_id": None,
+                    "date": 1678901234,
+                    "rating": 5.0,
+                    "text": "Отличный сервис!",
+                    "answer": "Спасибо за отзыв!",
+                },
+                ...
+            ]
             ```
 
             **Вывод с Telegram:**
             ```
-            {
-                "service_id": 4
-                "name": "123",
-                "additional_id": "457",
-                "date": 1678901234,
-                "rating": None,
-                "text": "Отличный сервис!",
-                "answer": None,
-            }
+            [
+                {
+                    "service_id": 4
+                    "name": "nickname",
+                    "additional_id": "457",
+                    "date": 1678901234,
+                    "rating": None,
+                    "text": "Отличный сервис!",
+                    "answer": None,
+                },
+                ...
+            ]
             ```
 
         """
