dbb540edea2e223921cf8815c3489198fa1f7f51
Artemii <angap4@gmail.com>
2025-05-08 20:35:08 +1000

    Update otzovik.py

--- get_info/otzovik/otzovik.py
+++ get_info/otzovik/otzovik.py
@@ -13,6 +13,7 @@ from get_info.abstract import Parser
 class OtzovikParser(Parser):
     def __init__(self):
         super().__init__(2)
+        
     def parse(
         self,
         q: str | list[str],
