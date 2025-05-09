e755f19556475dc9c331266e41034d9ac0d7bc31
Artemii <angap4@gmail.com>
2025-05-09 10:43:25 +1000

    work started

--- get_info/master_parser.py
+++ get_info/master_parser.py
@@ -1,5 +1,30 @@
-from .yandex_maps import YandexParser
-from .vk import VKParser
-from .telegram import TelegramParser
-from .google_maps import GoogleMapsParser
-from .otzovik import OtzovikParser
+"""
+Пайплайн парсинга.
+"""
+
+from .abstract import Parser
+
+
+class MasterParser:
+    def __init__(
+        self,
+        *services: Parser,
+        **parameters: dict[str, dict[str, str | list[str] | int]]
+    ) -> None:
+        """
+        Сервисы передаются экземплярами!
+
+        Параметры передаются лишь уникальные, по типу запросов, id и т. д., что будет отличаться от сервиса к сервису.
+        """
+        self.__serviceList = services
+        self.__parseParameters = parameters
+
+    def parse(self, **parameters) -> list[dict[str, str | int | float | None]]:
+        """
+        Параметры такие же, как и в parse у Parser
+        """
+        final_result = []
+        for service in self.__serviceList:
+            result = service.parse(**parameters)
+            final_result.extend(result)
+        return final_result
