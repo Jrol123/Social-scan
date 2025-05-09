b7cbfffde513819d5d984861ca89ef8bfa81b281
Artemii <angap4@gmail.com>
2025-05-09 12:15:33 +1000

    Master-parser done

--- get_info/master_parser.py
+++ get_info/master_parser.py
@@ -7,14 +7,14 @@ from .abstract import Parser
 
 class MasterParser:
     def __init__(
-        self,
-        *services: Parser,
-        **parameters: dict[str, str | list[str] | int]
+        self, *services: Parser, **parameters: dict[str, str | list[str] | int]
     ) -> None:
         """
         Сервисы передаются экземплярами!
 
         Параметры передаются лишь уникальные, по типу запросов, id и т. д., что будет отличаться от сервиса к сервису.
+
+        Параметры должны быть вида `__class__.name__ = {"param": val}`
         """
         self.__serviceList = services
         self.__parseParameters = parameters
@@ -25,6 +25,9 @@ class MasterParser:
         """
         final_result = []
         for service in self.__serviceList:
-            result = service.parse(**parameters)
+            parseParameters = self.__parseParameters[service.__class__.__name__]
+            result = service.parse(
+                **parseParameters, **parameters
+            )
             final_result.extend(result)
         return final_result
