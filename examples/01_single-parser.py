3e0fc7f0e7b43263ebb30b41e0331690ebf532a8
Artemii <angap4@gmail.com>
2025-05-16 20:59:31 +1000

    Update 01_single-parser.py

--- examples/01_single-parser.py
+++ examples/01_single-parser.py
@@ -1,3 +1,8 @@
+"""
+Этот пример посвящён парсингу, первому этапу в pipeline.
+
+В данном примере показано, как работать с парсерами по-отдельности.
+"""
 from src.get_info.parsers.yandex_maps import YandexMapsParser, YandexMapsConfig
 from src.get_info.core import MasterParserConfig
 
