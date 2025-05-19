094126b6f3869bbfc1eafef72e20708bdc33ba6e
Artemii <angap4@gmail.com>
2025-05-19 23:51:05 +1000

    report done?

---
+++ examples/05_report.py
@@ -0,0 +1,11 @@
+from dotenv import dotenv_values
+from pandas import read_csv
+from src.get_report import form_report
+
+if __name__ == "__main__":
+    secrets = dotenv_values()
+    
+    summaries = read_csv("examples/04_clusterization/clustered_summaries2.csv", index_col=0)
+    clusters = read_csv("examples/04_clusterization/categories.csv", index_col=0)
+    
+    form_report(summaries, clusters, secrets["CHUTES_API_TOKEN"], "examples/report.pdf")
